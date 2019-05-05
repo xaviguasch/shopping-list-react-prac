cc737a9d1a7855ee14c14c462366efeda2de95b9
xaviguasch <xaviguasch@gmail.com>
2019-05-05 15:58:10 +0200

    added uuid packages to generate IDs

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import ShoppingListForm from './ShoppingListForm'
+import uuid from 'uuid/v4'
 
 class ShoppingList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      items: [{ name: 'Milk', qty: '2 gallons' }, { name: 'Bread', qty: '2 loaves' }]
+      items: [{ name: 'Milk', qty: '2 gallons', id: uuid() }, { name: 'Bread', qty: '2 loaves', id: uuid() }]
     }
 
     this.addItem = this.addItem.bind(this)
   }
 
   addItem(item) {
+    let newItem = { ...item, id: uuid() }
     this.setState(st => ({
-      items: [...st.items, item]
+      items: [...st.items, newItem]
     }))
   }
 
@@ -21,7 +23,7 @@ class ShoppingList extends Component {
     return (
       <ul>
         {this.state.items.map(item => (
-          <li>
+          <li key={item.id}>
             {item.name}:{item.qty}
           </li>
         ))}
